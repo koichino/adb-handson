1f783f4440a2e09265dc892173aba15bb5b3e1b2
koichino <koichino@microsoft.com>
2025-12-16 08:40:29 +0000

    Serlverless クラスターのため、triggerを明示的に追加
    https://learn.microsoft.com/ja-jp/azure/databricks/compute/serverless/limitations#streaming
    
    ストリーミングの制限事項
    既定または時間ベースのトリガー間隔はサポートされません。 サポートされるのは Trigger.AvailableNow のみです。 「構造化ストリーミングのトリガー間隔を構成する」を参照してください。

--- Lakeflow/01_Medallion Architecture.py
+++ Lakeflow/01_Medallion Architecture.py
@@ -131,6 +131,7 @@ display(df.limit(10))
       .option("checkpointLocation", f"{sample_dataset_path}/checkpoints/orders_bronze") # チェックポイント格納先（増分に対する Exactly-Once Ingest 保証のため）
       .outputMode("append") # 読み取った増分データを追記することを指示
 #     .trigger(processingTime='500 milliseconds' # ストリームソースの読み取りとターゲットへの出力を 500 ms ごとに再実行（既定値）
+      .trigger(availableNow=True) #Serverlessの場合明示的な指定が必要
       .table("01_bronze_orders")) # 出力先テーブル
 
 # COMMAND ----------
@@ -261,6 +262,7 @@ load_new_data()
       .option("checkpointLocation", f"{sample_dataset_path}/checkpoints/orders_silver") # チェックポイント格納先（増分に対する Exactly-Once Ingest 保証のため）
       .outputMode("append") # 読み取った増分データを追記することを指示
 #     .trigger(processingTime='500 milliseconds' # ストリームソースの読み取りとターゲットへの出力を 500 ms ごとに再実行（既定値）
+      .trigger(availableNow=True) #Serverlessの場合明示的な指定が必要
       .table("01_silver_orders")) # 出力先テーブル
 
 # COMMAND ----------
